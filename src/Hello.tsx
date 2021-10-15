699febe071abc66a443f911b715fde25ea502286
Freewind <nowindlee@gmail.com>
2021-10-15 16:58:47 +0800

    add pcss

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
+import './Hello.pcss';
 
 type Props = {};
 
 export const Hello: FC<Props> = ({}) => {
-  return <div>
+  return <div className={'Hello'}>
     <h1>Hello React</h1>
   </div>;
 }
