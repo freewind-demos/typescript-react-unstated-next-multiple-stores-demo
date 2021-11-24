abe1b5d972603025433cd39ffffb3fed832a6722
Freewind <nowindlee@gmail.com>
2021-11-24 17:22:36 +0800

    fix vite refreshing

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Hello } from './hello'
+import { Hello } from './Hello'
 
 ReactDOM.render(
   <Hello/>,
-  document.body
+  document.getElementById('root')
 )
